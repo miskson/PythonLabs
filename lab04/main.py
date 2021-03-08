859b575e94c58a2fc61f624c0426edc8e283f1cb
miskosn <sergejlevsna@gmail.com>
2021-03-08 23:17:40 +0200

    reworked some code logick. Changed dict to tuple list.

--- lab04/main.py
+++ lab04/main.py
@@ -1,27 +1,26 @@
 import random
+from collections import namedtuple
 
 
-def showall(dictionary):
+def showall(persons):
+    for person in persons:
+        print(person.surname, ": salary -", person.salary, ", sex -", person.sex)
     print("*****")
-    for key in dictionary:
-        print(key, dictionary[key])
-    print("*****")
 
 
-def showallsorted(dictionary):
-    keylist = list(dictionary.keys())
-    keylist.sort()
-    for i in keylist:
-        print(i, ':', dictionary[i])
+def showallsorted(base):
 
+    base.sort(key=lambda Person: Person.surname)
+    print("Sorted list:")
+    showall(base)
 
 def deletekey(base):
     keyname = input("Input key you want to delete(or input 'q' to exit): ")
     if quitinput(keyname): return False
-    for key in base:
-        if keyname == key:
-            del base[key]
-            print("/Key has been deleted.")
+    for person in base:
+        if keyname == person.surname:
+            del base[base.index(person)]
+            print("-Key has been deleted.")
             return True
     print("There is no such key in base.")
     return False
@@ -38,13 +37,12 @@ def updatebase(base):
         sex = input("1 - male / 2 - female: ")
         if quitinput(sex): return False
         status = validateinfo(surname, salary, sex)
-
     salary = round(float(salary), 2)
     if sex == "1":
         sex = 'male'
     elif sex == "2":
         sex = 'female'
-    base.update({surname: {'salary': salary, 'sex': sex}})
+    base.append(Person(surname, salary, sex))
     print("/Base has been updated.")
 
 
@@ -80,19 +78,26 @@ def validateinfo(surname, salary, sex):
         return False
 
 
-surnames = ['Joebob', 'Dude', 'Bill', 'Mann', 'Xuan', 'Weaver', 'Pavlov', 'Shlapic', 'Anderson', 'Smith']
-base = {}
-for i in range(len(surnames)):
-    sex = random.randint(0, 1)
-    if sex == 0:
-        sex = 'male'
-    else:
-        sex = 'female'
-    base.update({surnames[i]: {'salary': round(random.uniform(200, 500), 2), 'sex': sex}})
+def getlowestsalaryMan(base):
+    minsalaryMan = min(base, key=lambda person: person.salary)
+    print("Minimal salary belongs to - ", minsalaryMan.surname, ": salary -", minsalaryMan.salary, ", sex -", minsalaryMan.sex)
+
+
+Person = namedtuple('Person', ('surname', 'salary', 'sex'))
+surnames = ['Joebob', 'King', 'Bill', 'Mann', 'Ripley', 'Weaver', 'Hicks', 'Nukem', 'Anderson', 'Smith']
+sex = ('male', 'female')
+#base = [Person(random.choice(surnames), round(random.uniform(200, 500)), random.choice(sex)) for _ in range(10)]
+base = [Person(surnames[i], round(random.uniform(200, 500), 2), random.choice(sex)) for i in range(10)]
 showall(base)
-updatebase(base)
-showall(base)
-deletekey(base)
-showall(base)
-showallsorted(base)
-#hi
+# showallsorted(base)
+# deletekey(base)
+# showall(base)
+#updatebase(base)
+#showall(base)
+getlowestsalaryMan(base)
+#showall(base)
+
+
+
+
+
