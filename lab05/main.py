ce3b19feceecf08d643dc916c922af6fd23314db
miskosn <sergejlevsna@gmail.com>
2021-03-31 09:28:05 +0300

    some code has been cleared.

--- lab05/main.py
+++ lab05/main.py
@@ -33,13 +33,6 @@ def isnotsign(string):
         return True
 
 
-def customsplit(string):
-    string = string.split()
-    for i in string:
-        re.split(r"[^-'\w]", string)
-    return string
-
-
 thestring = input("input string: ")
 if validatestring(thestring):
     stringarr = list(filter(None, re.split(r"[^-'\w]", thestring)))
@@ -48,28 +41,23 @@ if validatestring(thestring):
 
     couples = []
     coincidences = 0
-    coincidenceInd = 0
-    couplesnum = 0
     for i in stringarr:
         for k in stringarr:
             if i == k:
                 coincidences += 1
-                coincidenceInd = k
 
         if coincidences > 2 or coincidences <= 1:
             coincidences = 0
-            coincidenceInd = 0
         elif coincidences == 2:
             couples.append(i)
             coincidences = 0
-            coincidenceInd = 0
 
     print("Pairs of words that are similar it the string: ", couples)
 
 else:
     print("String contains forbidden characters or empty")
 
-#2.A
+# 2.A
 consonants = re.compile(r"[bcdfghklmnpqrstvwxz]")
 thetext = input("Input your text: ")
 print(thetext)
@@ -80,13 +68,14 @@ if len(thetext) > 3:
     consonatscount = 0
 
     for word in thetext:
-        #checking if the word contains eng letters
+        # checking if the word contains eng letters
         if re.match(r"[a-zA-Z0-9,-.:;'`\"[\]()!? ]", word):
             for i in word:
-                #counting consonants in the word
+                # counting consonants in the word
                 if re.match(consonants, i.lower()):
                     consonatscount += 1
-            #adding current word to the named tuple that contains word information
+
+            # adding current word to the named tuple that contains word information
             wordsinfo.append(Word(word, consonatscount, thetext.index(word)))
             consonatscount = 0
         else:
@@ -117,13 +106,12 @@ if len(thetext) > 3:
     print("the text after words being deleted:")
     thetext = ''.join(thetext)
     print(thetext)
-
 else:
     print("the string contains forbidden characters or less than 3 words.")
 
-#2.B
+# 2.B
 thetext = input("Input new text: ")
-if thetext and validatetextEn(thetext) and validatestring(thetext):
+if validatetextEn(thetext) and validatestring(thetext):
     thetext = thetext.lower()
     letterinfo = []
     onceletters = []
@@ -143,8 +131,10 @@ if thetext and validatetextEn(thetext) and validatestring(thetext):
         elif i.quantity == 1:
             oneletter = i.lettername
             onceletters.append(oneletter)
+
     multipleletters = list(filter(isnotsign, multipleletters))
     onceletters = list(filter(isnotsign, onceletters))
     print("Letters that occure only once in the text: ", onceletters)
     print("Letters that occure two or more times in the text: ", multipleletters)
-else: print("there is no text or it contains forbidden characters")
+else:
+    print("there is no text or it contains forbidden characters")
