74b481c741233a1585de93aaabb53cdad168ff70
miskosn <sergejlevsna@gmail.com>
2021-05-08 16:33:10 +0300

    lab09 is done.

--- lab09/main.py
+++ lab09/main.py
@@ -1,21 +1,33 @@
-# class Person:
-#     name = "Bob"
-#     age = 25
-#     employed = True
-#     responsabilities = ['Cooking', 'washing dishes', 'cleaning']
-#
-#     def greet(self):
-#         print("Hello, my name is ", self.name, " I'm ", self.age, "years old.")
-#         if self.employed is True:
-#             respstring = self.responsabilities
-#             respstring = ",".join(respstring)
-#             print("My working responsabilities are: ", respstring)
-#         else:
-#             print("I'm currently unemployed.")
+def objectcheck(func):
+    def inner(obj):
+        if type(obj) != object:
+            return print("given value is not an object!")
+        try:
+            func(obj)
+        except:
+            return print("an error occurred")
 
-class Person():
+    return inner
 
-    def __init__(self, name, age, employmentstatus, responsabilities, wage, workdays, workhours):
+
+@objectcheck
+def generateobjectfile(obj):
+    with open(f"{obj.__class__.__name__}__{obj.name}.txt", "w+") as f:
+        strings = str(obj)
+        f.write(strings)
+
+
+class Person:
+    name = str
+    age = int
+    emoloymentstatus = bool
+    responsabilities = list()
+    wage = float
+    workdays = int
+    workhours = int
+    monthincome = float
+
+    def __init__(self, name, age, employmentstatus=False, responsabilities=None, wage=0, workdays=0, workhours=0):
         self.name = name
         self.age = age
         self.employmentstatus = employmentstatus
@@ -23,6 +35,24 @@ class Person():
         self.wage = wage
         self.workdays = workdays
         self.workhours = workhours
+        self.income = f"My monthly income is: {self.monthlyincome()}. My year income is: {self.yearlyincome()}"
+
+    # for computer representation
+    def __repr__(self):
+        return f"{self.name} is The object of class {self.__class__.__name__} stored ad {hex(id(self))}"
+
+    # for humans
+    def __str__(self):
+        return f"class {self.__class__.__name__}:\n" \
+               f"   name: {self.name}\n" \
+               f"   age: {self.age}\n" \
+               f"   employmentstatus: {self.employmentstatus}\n" \
+               f"   responsabilities: {self.responsabilities}\n" \
+               f"   wage: {self.wage}\n" \
+               f"   workdays: {self.workdays}\n" \
+               f"   workhours: {self.workhours}\n" \
+               f"   income: {self.income}\n" \
+
 
     def greet(self):
         print("Hello, my name is ", self.name, " I'm ", self.age, "years old.")
@@ -37,11 +67,17 @@ class Person():
         monthincome = ((self.wage * self.workhours) * self.workdays) * 3
         return monthincome
 
+    def yearlyincome(self):
+        yearincome = self.monthlyincome() * 12
+        return yearincome
+
 
 person1 = Person("Sergei", "20", True,
                  list(['mail loading/unloading', 'mail receiving/delivery', 'workplace cleaning']),
                  45, 5, 4)
 person1.greet()
-print("mounthly income is:", person1.monthlyincome())
-person2 = Person("David", "19", False, list(['studying']), 0, 0, 0)
+print(person1.income)
+
+person2 = Person("David", "19", False)
 person2.greet()
+print(person2.responsabilities, person2.wage, person2.workdays, person2.workhours)
