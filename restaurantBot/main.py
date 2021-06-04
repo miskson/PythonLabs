a94d02966b67f240d12b1c85584e918770439f38
miskosn <sergejlevsna@gmail.com>
2021-06-04 10:22:33 +0300

    D:/Git/start command rewritten

--- restaurantBot/main.py
+++ restaurantBot/main.py
@@ -6,8 +6,11 @@ bot = telebot.TeleBot(config.TOKEN)
 @bot.message_handler(commands=['start'])
 def welcome_message(message):
     bot.reply_to(message, f"Hello {message.from_user.first_name}\n\n"
-                          f"I am created to help you locating nearby restaurants and dines\n\n"
-                          f"Also I save list of places you decided to add\n\n")
+                          f"I am created to help you remeber and locate nearby restaurants and cafes you like.\n\n"
+                          f"/add - to add restaurant's name photo and location you would like to save.\n\n"
+                          f"/list - to see the list of all saved palaces.\n\n"
+                          f"/reset - to reset the list(this action will delete all previously saved locations).\n\n"
+                          f"/nearby - to locate all of the restaurants near you in 500m radius.")
     print(message.from_user)
 
 bot.polling(none_stop=True)
